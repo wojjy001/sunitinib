28762e1e1fd5b119311b2b940e72585c18a18d1b
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2017-07-20 16:22:33 -0400

    Tab formatting

--- Project/ModelFiles/yu_2015_sunitinib_model.R
+++ Project/ModelFiles/yu_2015_sunitinib_model.R
@@ -30,102 +30,105 @@
 # ------------------------------------------------------------------------------
 # Define model
 pk.code <- '
-$SET		atol = 1e-8, rtol = 1e-8
-        maxsteps = 100000
+$SET			atol = 1e-8, rtol = 1e-8
+          maxsteps = 100000
 
-$INIT		// Initial conditions for PK compartments
-        // Parent, sunitinib
-        DEPOT = 0,	// Depot compartment for dose
-        CENTP = 0,	// Central compartment
+$INIT			// Initial conditions for PK compartments
+          // Parent, sunitinib
+          DEPOT = 0,	// Depot compartment for dose
+          CENTP = 0,	// Central compartment
 
-        // Metabolite, SU12662
-        CENTM = 0,	// Central compartment
-        PERIM = 0,	// Peripheral compartment
+          // Metabolite, SU12662
+          CENTM = 0,	// Central compartment
+          PERIM = 0,	// Peripheral compartment
 
-        // Area under the curve
-        AUC = 0	// Combined parent and metabolite
+          // Area under the curve
+          AUC = 0	// Combined parent and metabolite
 
-$PARAM	// Population parameters
-        // Parent, sunitinib
-        POPKA = 0.34,	// Absorption rate constant, h^-1
-        POPCLP = 35.7,	// Clearance, L/h
-        POPVCP = 1360,	// Central volume, L
-        POPQH = 80,	// Hepatic blood flow, L/h
+$PARAM		// Population parameters
+          // Parent, sunitinib
+          POPKA = 0.34,	// Absorption rate constant, h^-1
+          POPCLP = 35.7,	// Clearance, L/h
+          POPVCP = 1360,	// Central volume, L
+          POPQH = 80,	// Hepatic blood flow, L/h
 
-        // Metabolite, SU12662
-        POPCLM = 17.1, // Clearance, L/h
-        POPVCM = 635,	// Central volume, L
-        POPQI = 20.1,	// Inter-compartmental clearance, L/h
-        POPVPM = 388,	// Peripheral volume, L
+          // Metabolite, SU12662
+          POPCLM = 17.1, // Clearance, L/h
+          POPVCM = 635,	// Central volume, L
+          POPQI = 20.1,	// Inter-compartmental clearance, L/h
+          POPVPM = 388,	// Peripheral volume, L
 
-        // Pre-defined between subject variability
-        ETACLP = 0,	// ETA on clearance of parent
-        ETAVCP = 0, // ETA on central volume of parent
-        ETACLM = 0, // ETA on clearance of metabolite
-        ETAVCM = 0, // ETA on central volume of metabolite
+          // Pre-defined between subject variability
+          ETACLP = 0,	// ETA on clearance of parent
+          ETAVCP = 0, // ETA on central volume of parent
+          ETACLM = 0, // ETA on clearance of metabolite
+          ETAVCM = 0, // ETA on central volume of metabolite
 
-        // Covariates
-        WT = 70,	// Body weight, kg
-        OBASE = 200,	// Observed tumour size at baseline (length of longest diameter, mm)
-        HFSBASE = 0,	// Baseline hand-foot syndrome grade
-        FATBASE = 0	// Baseline fatigue grade
+          // Covariates
+          WT = 70,	// Body weight, kg
+          OBASE = 200,	// Observed tumour size at baseline (length of longest diameter, mm)
+          HFSBASE = 0,	// Baseline hand-foot syndrome grade
+          FATBASE = 0	// Baseline fatigue grade
 
-$OMEGA	// Between-subject variability
-        @annotated @block
-        BSVCLP:	0.114921 : ETA on clearance of parent
-        BSVVCP:	0 0.104976 : ETA on central volume of parent
-        BSVCLM:	0.07564107 0 0.1777241 : ETA on clearance of metabolite
-        BSVVCM:	0 0.09004608 0.10969155 0.335241 : ETA on central volume of metabolite
+$OMEGA		// Between-subject variability
+          @annotated @block
+          BSVCLP:	0.114921 : ETA on clearance of parent
+          BSVVCP:	0 0.104976 : ETA on central volume of parent
+          BSVCLM:	0.07564107 0 0.1777241 : ETA on clearance of metabolite
+          BSVVCM:	0 0.09004608 0.10969155 0.335241 : ETA on central volume of metabolite
 
-$SIGMA	// Random unexplained variability
-        @annotated
-        ERRPROP1:	0.06 : Proportional error for parent in study 1
-        ERRPROP2: 0.01 : Proportional error for parent in study 2 and study 3
-        ERRPROM1: 0.03 : Proportional error for metabolite in study 1
-        ERRPROM2: 0.01 : Proportional error for metabolite in study 2 and study 3
+$SIGMA		// Random unexplained variability
+          @annotated
+          ERRPROP1:	0.06 : Proportional error for parent in study 1
+          ERRPROP2: 0.01 : Proportional error for parent in study 2 and study 3
+          ERRPROM1: 0.03 : Proportional error for metabolite in study 1
+          ERRPROM2: 0.01 : Proportional error for metabolite in study 2 and study 3
 
-$MAIN		// Individual PK parameter values
-        // Parent, sunitinib
-        double CLP = POPCLP*pow(WT/70,0.75)*exp(ETACLP);
-        double VCP = POPVCP*pow(WT/70,1)*exp(ETAVCP);
-        double QH = POPQH*pow(WT/70,0.75);
+$MAIN			// Individual PK parameter values
+          // Parent, sunitinib
+          double CLP = POPCLP*pow(WT/70,0.75)*exp(ETACLP);
+          double VCP = POPVCP*pow(WT/70,1)*exp(ETAVCP);
+          double QH = POPQH*pow(WT/70,0.75);
 
-        // Metabolite, SU12662
-        double CLM = POPCLM*pow(WT/70,0.75)*exp(ETACLM);
-        double VCM = POPVCM*pow(WT/70,1)*exp(ETAVCM);
-        double QI = POPQI*pow(WT/70,0.75);
-        double VPM = POPVPM*pow(WT/70,1);
+          // Metabolite, SU12662
+          double CLM = POPCLM*pow(WT/70,0.75)*exp(ETACLM);
+          double VCM = POPVCM*pow(WT/70,1)*exp(ETAVCM);
+          double QI = POPQI*pow(WT/70,0.75);
+          double VPM = POPVPM*pow(WT/70,1);
 
-        // Hepatic extraction
-        double FH = QH/(QH+CLP);	// Parent fraction not metabolised by liver
-        double EH = 1-FH;	// Parent fraction metabolised by liver
-        double FM = 0.21;	// Parent fraction metabolised by liver to metabolite
+          // Hepatic extraction
+          double FH = QH/(QH+CLP);	// Parent fraction not metabolised by liver
+          double EH = 1-FH;	// Parent fraction metabolised by liver
+          double FM = 0.21;	// Parent fraction metabolised by liver to metabolite
 
-        // Micro-rate constants
-        double K12 = POPKA;
-        double K20 = CLP/VCP;
-        double K23 = QH/VCP;
-        double K30 = CLM/VCM;
-        double K34 = QI/VCM;
-        double K43 = QI/VPM;
+          // Micro-rate constants
+          double K12 = POPKA;
+          double K20 = CLP/VCP;
+          double K23 = QH/VCP;
+          double K30 = CLM/VCM;
+          double K34 = QI/VCM;
+          double K43 = QI/VPM;
 
-$ODE		// Parent, sunitinib
-        dxdt_DEPOT = -K12*DEPOT;
-        dxdt_CENTP = FH*(K12*DEPOT +K23*CENTP) -K23*CENTP;
+$ODE			// Parent, sunitinib
+          dxdt_DEPOT = -K12*DEPOT;
+          dxdt_CENTP = FH*(K12*DEPOT +K23*CENTP) -K23*CENTP;
 
-        // Metabolite, SU12662
-        dxdt_CENTM = FM*EH*(K12*DEPOT +K23*CENTP) -K34*CENTM +K43*PERIM -K30*CENTM;
-        dxdt_PERIM = K34*CENTM -K43*PERIM;
+          // Metabolite, SU12662
+          dxdt_CENTM = FM*EH*(K12*DEPOT +K23*CENTP) -K34*CENTM +K43*PERIM -K30*CENTM;
+          dxdt_PERIM = K34*CENTM -K43*PERIM;
 
-        // Area under the curve
-        dxdt_AUC = CENTP/VCP+CENTM/VCM;	// Combined parent and metabolite
+          // Area under the curve
+          dxdt_AUC = CENTP/VCP+CENTM/VCM;	// Combined parent and metabolite
 
-$TABLE	// Calculate parent and metabolite concentrations
-        double IPREP = CENTP/VCP;	// mg/L
-        double IPREM = CENTM/VCM;	// mg/L
-        double IPRE = IPREP+IPREM;	// mg/L
+$TABLE		// Calculate parent and metabolite concentrations
+          double IPREP = CENTP/VCP;	// mg/L
+          double IPREM = CENTM/VCM;	// mg/L
+          double IPRE = IPREP+IPREM;	// mg/L
+          double DV = IPRE*(1+ERRPROP1);	// mg/L
 
-$CAPTURE WT OBASE HFSBASE FATBASE IPREP IPREM IPRE
+$CAPTURE	WT OBASE HFSBASE FATBASE
+          IPREP IPREM IPRE DV
+          CLP VCP CLM VCM QI VPM
 '
 # Compile the model code
   pk.mod <- mcode("popPK",pk.code)
